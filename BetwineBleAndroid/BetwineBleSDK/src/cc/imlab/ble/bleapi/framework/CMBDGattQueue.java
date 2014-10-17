94f306ca20cb5445c427b506b5ae8308aadbdc6a
terryoy <terry.ouyang@gmail.com>
2014-10-17 17:44:22 +0800

    recommit queue class

---
+++ BetwineBleAndroid/BetwineBleSDK/src/cc/imlab/ble/bleapi/framework/CMBDGattQueue.java
@@ -0,0 +1,139 @@
+package cc.imlab.ble.bleapi.framework;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.UUID;
+
+import cc.imlab.ble.bleapi.BetwineCMDefines;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattDescriptor;
+import android.os.Handler;
+import android.util.Log;
+
+
+/**
+ * This Gatt Queue is specific for Android, because it cann't handle 
+ * multiple Read/Write operations at the same time
+ * 
+ * @author Terry Ouyang
+ *
+ */
+public class CMBDGattQueue {
+	
+	private BluetoothGatt gatt;
+	
+	public Boolean isProcessing;
+	public Queue<CMBDGattCommand> queue;
+	
+	public CMBDGattQueue() {
+		queue = new LinkedList<CMBDGattQueue.CMBDGattCommand>();
+		isProcessing = false;
+	}
+	
+	public enum CMBDGattCommandType {
+		Read,
+		Write,
+		Notify,
+	}	
+	
+	public class CMBDGattCommand {
+		BluetoothGattCharacteristic characteristic;
+		CMBDGattCommandType operation;
+		byte[] writeValue;
+		Boolean notifyValue;
+		
+		public CMBDGattCommand(CMBDGattCommandType operation,
+				BluetoothGattCharacteristic characteristic) {
+			this.characteristic = characteristic;
+			this.operation = operation;
+		}
+		
+		void executeWithGatt(BluetoothGatt gatt) {
+			
+			switch(operation) {
+			case Read:
+				gatt.readCharacteristic(characteristic);
+				
+				break;
+				
+			case Write:
+		    	characteristic.setValue(writeValue);
+		    	characteristic.setWriteType(BluetoothGattCharacteristic.WRITE_TYPE_DEFAULT);
+		    	gatt.writeCharacteristic(characteristic);
+		    	
+				break;
+				
+			case Notify:
+				gatt.setCharacteristicNotification(characteristic, notifyValue);
+				
+				BluetoothGattDescriptor keyDescriptor = characteristic.getDescriptor(UUID.fromString(BetwineCMDefines.CB_CHAR_UUID_KEY_DESCRIPTOR));
+				keyDescriptor.setValue(BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE);
+	        	gatt.writeDescriptor(keyDescriptor);
+				
+				break;
+			
+			default:
+				break;
+			}
+		}
+		
+	}
+	
+	void addReadCommand(BluetoothGattCharacteristic characteristic) {
+		CMBDGattCommand command = new CMBDGattCommand(CMBDGattCommandType.Read, characteristic);
+		this.queue.add(command);
+	}
+	
+	void addWriteCommand(BluetoothGattCharacteristic characteristic, byte[] writeVaule) {
+		CMBDGattCommand command = new CMBDGattCommand(CMBDGattCommandType.Write, characteristic);
+		command.writeValue = writeVaule;
+		this.queue.add(command);
+	}
+	
+	void addNotifyCommand(BluetoothGattCharacteristic characteristic, Boolean boolValue) {
+		CMBDGattCommand command = new CMBDGattCommand(CMBDGattCommandType.Read, characteristic);
+		command.notifyValue = boolValue;
+		this.queue.add(command);
+	}
+	
+	void setGatt(BluetoothGatt gatt) {
+		this.gatt = gatt;
+		
+		if (gatt == null) {
+			queue.clear();
+		}
+	}
+	
+	void checkProceedQueue() {
+		
+		if (isProcessing || gatt == null) {
+			return;
+		}
+		
+		// procceed queue
+		if (queue.size() > 0) {
+			this.isProcessing = true;
+			this.executeNextQueueCommand();
+		}
+	}
+	
+	void executeNextQueueCommand() {
+		
+		if (queue.size() > 0) {
+			this.isProcessing = true;
+			CMBDGattCommand command = queue.poll();
+			command.executeWithGatt(gatt);
+	
+			Log.i("CMBDGattQueue", "execute command: " + command.operation);
+		
+		}
+		else {
+			isProcessing = false;
+		}
+		
+	}
+	
+	
+}
--- BetwineBleAndroid/BetwineBleSDK/src/cc/imlab/ble/bleapi/framework/CMBDPeripheralConnector.java
+++ BetwineBleAndroid/BetwineBleSDK/src/cc/imlab/ble/bleapi/framework/CMBDPeripheralConnector.java
@@ -62,6 +62,7 @@ public abstract class CMBDPeripheralConnector extends BluetoothGattCallback {
 			gattQueue.setGatt(null);
 			
 			gatt.close();
+			keepConnection = false;
 		}
 	}
 	
